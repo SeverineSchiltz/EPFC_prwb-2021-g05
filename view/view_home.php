a0169882d3c27ea31c80b3f3b89ca8d6d598ee21
Severine Schiltz <schiltz.severine@gmail.com>
2020-12-14 19:16:18 +0100

    feat(Home) : ajout du code de départ

---
+++ view/view_home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Board</title>
+        <base href="<?= $web_root ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="title"> </div>
+        <div class="menu">
+        Hello guest ! Please
+            <a href="main/login">Log In</a>
+            or
+            <a href="main/signup">Sign Up</a>
+        </div>
+    </body>
+</html>
