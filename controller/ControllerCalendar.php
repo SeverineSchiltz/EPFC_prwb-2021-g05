b3a298b68027ef2fb75f3799cee23d8caacc5d48
Sinouhe <sinou.94@hotmail.fr>
2021-05-22 20:32:14 +0200

    feat(Calendar): update boards on click AJAXified almost works

--- controller/ControllerCalendar.php
+++ controller/ControllerCalendar.php
@@ -19,11 +19,24 @@ class ControllerCalendar extends Controller {
         $user = $this->get_user_or_redirect();
         $calendar = new Calendar($user);
         if ($this->user_logged()) {
-            (new View("calendar"))->show(array("user" => $user, "boards" => $calendar->get_boards_with_events()));
+            (new View("calendar"))->show(array("user" => $user, "boards" => $calendar->get_boards()));
         } else {
             (new View("home"))->show();
         }
     }
 
+    public function get_events_service() {
+        $res = "false";
+        if(isset($_POST["boards"]) && $_POST["boards"]){
+            $boards_js = json_decode($_POST["boards"]);
+            // echo json_encode($board_ids);
+            $user = $this->get_user_or_false();
+            if($user) {
+                $calendar = new Calendar($user);
+                $res = $calendar->get_events($boards_js);
+            }
+        }
+        echo $res;
+    }
 }
 
